fe0ee940a3525779170636f6811d9d1c67233aba
YeJinMo <985189148@qq.com>
2016-11-11 22:04:19 +0800

    master
    master

---
+++ NetVideoPlayer/SearchEngine/Search_Engine_CI_LI_BA.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NetVideoPlayer.SearchEngine
+{
+    public class Search_Engine_CI_LI_BA
+    {
+
+        public List<Struct_BT_Search> Get_Search_CI_LI_BA(string KeyWord, int PageIndex, int TimeOut = 10000)
+        {
+            try
+            {
+                List<Struct_BT_Search> List_BT_Search = new List<Struct_BT_Search>();
+                WebClientFX web = new WebClientFX(TimeOut);
+                web.Encoding = Encoding.UTF8;
+                string HTML = web.DownloadString(string.Format(@"http://www.cili8.org/s/{0}_rel_{1}.html", KeyWord, PageIndex)).Replace("\r", "").Replace("\n", "").Replace("\t", "").Replace(" ", "").Replace("&nbsp;", "").Replace("<em>", "").Replace("</em>", "").Replace("class=\"cpillyellow-pill\"", "");
+                Regex regResult = new Regex("<ahref=\"http://www.cili8.net/detail/(.+?).html\"target=\"_blank\">(.+?)</a></h3></div><divclass=\"item-list\">(.+?)</div><divclass=\"item-bar\"><spanclass=\"cpillfileType1\">视频</span><span>创建时间：<b>(.+?)</b></span><span>文件大小：<b>(.+?)</b></span>");
+                MatchCollection mcResult = regResult.Matches(HTML);
+                if (mcResult.Count > 0)
+                {
+                    foreach (Match mc in mcResult)
+                    {
+                        string ToUrl = mc.Groups[1].Value;
+                        string Name = mc.Groups[2].Value;
+                        string UpTime = mc.Groups[4].Value;
+                        string VideoLength = mc.Groups[5].Value;
+                        Struct_BT_Search item = new Struct_BT_Search()
+                        {
+                            ToUrl = ToUrl,
+                            Name = Name.Replace("【失效链接】", "").Replace("【无效链接】", "").Replace("【处理中,请等待】", ""),
+                            UpTime = UpTime,
+                            VideoLength = VideoLength,
+                            Source = "CI_LI_BA"
+                        };
+                        List_BT_Search.Add(item);
+                    }
+                    //检测所有磁链连通性
+                    //TestingHash();
+                    return List_BT_Search;
+                }
+                else
+                {
+                    throw new Exception("No Data");
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+    }
+}
---
+++ NetVideoPlayer/SearchEngine/Struct_BT_Search.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetVideoPlayer.SearchEngine
+{
+    /// <summary>
+    /// BT搜索结构体
+    /// </summary>
+    public class Struct_BT_Search
+    {
+        /// <summary>
+        /// 详细地址
+        /// </summary>
+        public string ToUrl { get; set; }
+        /// <summary>
+        /// 名称 
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 更新时间
+        /// </summary>
+        public string UpTime { get; set; }
+        /// <summary>
+        /// 资源大小
+        /// </summary>
+        public string VideoLength { get; set; }
+        /// <summary>
+        /// 来源
+        /// </summary>
+        public string Source { get; set; }
+
+        private string testing = "检测中";
+        /// <summary>
+        /// 是否有效
+        /// </summary>
+        public string Testing
+        {
+            get { return testing; }
+            set { testing = value; }
+        }
+
+    }
+}
